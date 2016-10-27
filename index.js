b901b53ff64b5a4532004324d85313c87e0aecc7
ikonovalov <konovalov84@gmail.com>
2016-10-27 23:50:18 +0300

    Attach web3 and commander for a CLI

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-/**
- * Created by ikonovalov on 25/10/16.
- */
-module.exports = {
-    cryptoUtils: require('./lib/crypto-utils')
-};
+#!/usr/bin/env node
+'use strict';
+const program = require('commander');
+const web3_utils = require('./lib/web3-utils');
+const colors = require('colors');
+
+program
+    .version('0.0.1')
+    .command('accounts <command>')
+    .description('Ethereum accounts operations')
+    .option('-b, --balance', 'Show account balance')
+    .action((command, options) => {
+        if (command == 'ls') {
+            accounts_ls(options);
+        }
+    });
+
+program.parse(process.argv); // notice that we have to parse in a new statement.
+
+function accounts_ls(options) {
+    let coinbase = web3_utils.coinbase();
+    web3_utils.listAccounts().forEach(
+        account => {
+            let out = account;
+            if (options.balance) {
+                out += ` balance ${web3_utils.getBalance(account)} ETH`;
+            }
+            if (account == coinbase) {
+                console.log(`${out.red.bold} <- coinbase`)
+            } else {
+                console.log(out)
+            }
+        });
+}
