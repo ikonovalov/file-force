0a984b3a745d74ea6a872a7f072b8a87dffd9988
ikonovalov <konovalov84@gmail.com>
2016-10-28 00:29:31 +0300

    Add initial solidity contract

---
+++ solidity/FileForce.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.2;
+
+contract FileForce {
+
+    uint16 public constant version = 1;
+
+    struct Document {
+        
+        address author;
+
+        uint16 typeIndex;
+
+        LocationChallenge location; 
+    }
+
+    // encrypted location and other surprises
+    struct LocationChallenge {
+
+    }
+
+    event FileRegistred(address author);
+
+    event FileUnregistred(address autor); 
+
+    // constructor
+    function FileForce() {
+
+    }
+
+}
