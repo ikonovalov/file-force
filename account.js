cdf74d6a5184fdb64f995c005b6cf32f460f3869
ikonovalov <konovalov84@gmail.com>
2016-11-01 00:56:35 +0300

    Reorganize command structure and add private/public keys command

---
+++ account.js
@@ -0,0 +1,51 @@
+/**
+ * Created by ikonovalov on 01/11/16.
+ */
+
+const ethereum = require('./lib/web3-utils');
+const ethereumKeys = require('./lib/crypto-utils');
+const colors = require('colors');
+const validator = require('validator');
+
+module.exports = {
+    keys: (arg, options) => {
+        const numberInput = validator.isInt(arg);
+        const ethAccounts = ethereum.listAccounts();
+
+        if (arg > ethAccounts.length) {
+            console.log(`You have ${ethAccounts.length} accounts only. But your's input was ${arg}.`.red);
+            return;
+        }
+
+        const account = numberInput ? ethAccounts[arg] : arg;
+        if (numberInput) {
+            console.log(`Account ${account}`.red.bold)
+        }
+        const readlineSync = require('readline-sync');
+        let password = readlineSync.question('Unlock account. Password: ', {
+            hideEchoBack: true
+        });
+
+        let privateKey = ethereumKeys.recoverPrivateKey(require('path').join('/mnt/u110/ethereum', 'pnet1'), account, password);
+        console.log(`Private key:\t${privateKey.toString('hex')}`);
+
+        let publicKey = ethereumKeys.private2public(privateKey);
+        console.log(`Public key:\t${publicKey.toString('hex')}`)
+    },
+
+    ls: (arg, options) => {
+        let coinbase = ethereum.coinbase();
+        ethereum.listAccounts().forEach(
+            (account, index) => {
+                let out = `[${index}] ${account}`;
+                if (account == coinbase) {
+                    console.log(`${out.red.bold} <- coinbase`)
+                } else {
+                    console.log(out)
+                }
+                if (arg === 'balance') {
+                    console.log(`\tBalance ${ethereum.getBalance(account)} ETH`);
+                }
+            });
+    }
+};
--- cli.js
+++ cli.js
@@ -1,66 +1,44 @@
 #!/usr/bin/env node
 'use strict';
 const ethereum = require('./lib/web3-utils');
-const IPFS = require('./lib/ipfs-utils');
-const ipfs = new IPFS('/ip4/127.0.0.1/tcp/5001');
+const account = require('./account');
+const ipfs = require('./ipfs');
 const colors = require('colors');
 const program = require('commander');
+const validator = require('validator');
 
 program
     .version('0.0.1')
-    .command('accounts <command>')
-        .description('Ethereum accounts operations. Commands: ls')
-        .option('-b, --balance', 'Show account balance')
-        .action((command, options) => {
-            switch(command) {
-                case 'ls': {
-                    accounts_ls(options);
-                    break;
-                }
-                default: {
-                    console.log(`Command 'accounts ${command}' not found.`);
-                }
+    .command('account <command> [arg]')
+    .description('Ethereum account operations. Commands: ls, public')
+    .action((command, arg, options) => {
+        switch (command) {
+            case account.ls.name: {
+                account.ls(arg, options);
+                break;
             }
-        });
+            case account.keys.name: {
+                account.keys(arg, options);
+                break;
+            }
+            default: {
+                console.log(`Command 'accounts ${command}' not found.`);
+            }
+        }
+    });
 
 program.command('ipfs <command> [arg]')
     .description('IPFS files operations. Command: add')
     .action((command, arg) => {
-            switch(command) {
-                case 'add':{
-                    ipfs_add(arg);
-                    break;
-                }
+        switch (command) {
+            case ipfs.add.name: {
+                ipfs.add(arg);
+                break;
             }
+        }
 
-        });
+    });
 
 program.parse(process.argv); // notice that we have to parse in a new statement.
 
-function ipfs_add(path) {
-    ipfs.add(path, (error, result) => {
-        if (!error) {
-            result.forEach(rootBlock => {
-                console.log(`${rootBlock.path}  -> ${rootBlock.hash}`);
-            })
-        } else {
-            console.error(error);
-        }
-    });
-}
 
-function accounts_ls(options) {
-    let coinbase = ethereum.coinbase();
-    ethereum.listAccounts().forEach(
-        (account, index) => {
-            let out = `[${index}] ${account}`;
-            if (options.balance) {
-                out += ` Balance ${ethereum.getBalance(account)} ETH`;
-            }
-            if (account == coinbase) {
-                console.log(`${out.red.bold} <- coinbase`)
-            } else {
-                console.log(out)
-            }
-        });
-}
