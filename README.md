0eda392838afae38b122d70a4e3b15586ce7369b
ikonovalov <konovalov84@gmail.com>
2016-10-31 23:42:34 +0300

    add ipfs support

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Decentralized file sharing with:
 * IPFS - decentralized file store (or network).
 * Elliptic curve cryptography (curve: secp256k1)
 * ECDH (shared keys), HKDF (key derivation function) and AES-256-CTR
+
+#### License: LGPL-3.0
+#### Authors: [Igor Konovalov](https://github.com/ikonovalov)
--- cli.js
+++ cli.js
@@ -1,29 +1,61 @@
 #!/usr/bin/env node
 'use strict';
-const program = require('commander');
-const web3_utils = require('./lib/web3-utils');
+const ethereum = require('./lib/web3-utils');
+const IPFS = require('./lib/ipfs-utils');
+const ipfs = new IPFS('/ip4/127.0.0.1/tcp/5001');
 const colors = require('colors');
+const program = require('commander');
 
 program
     .version('0.0.1')
     .command('accounts <command>')
-    .description('Ethereum accounts operations')
-    .option('-b, --balance', 'Show account balance')
-    .action((command, options) => {
-        if (command == 'ls') {
-            accounts_ls(options);
-        }
-    });
+        .description('Ethereum accounts operations. Commands: ls')
+        .option('-b, --balance', 'Show account balance')
+        .action((command, options) => {
+            switch(command) {
+                case 'ls': {
+                    accounts_ls(options);
+                    break;
+                }
+                default: {
+                    console.log(`Command 'accounts ${command}' not found.`);
+                }
+            }
+        });
+
+program.command('ipfs <command> [arg]')
+    .description('IPFS files operations. Command: add')
+    .action((command, arg) => {
+            switch(command) {
+                case 'add':{
+                    files_ipfs_add(arg);
+                    break;
+                }
+            }
+
+        });
 
 program.parse(process.argv); // notice that we have to parse in a new statement.
 
+function files_ipfs_add(path) {
+    ipfs.add(path, (error, result) => {
+        if (!error) {
+            result.forEach(rootBlock => {
+                console.log(`${rootBlock.path}  -> ${rootBlock.hash}`);
+            })
+        } else {
+            console.error(error);
+        }
+    });
+}
+
 function accounts_ls(options) {
-    let coinbase = web3_utils.coinbase();
-    web3_utils.listAccounts().forEach(
-        account => {
-            let out = account;
+    let coinbase = ethereum.coinbase();
+    ethereum.listAccounts().forEach(
+        (account, index) => {
+            let out = `[${index}] ${account}`;
             if (options.balance) {
-                out += ` balance ${web3_utils.getBalance(account)} ETH`;
+                out += ` Balance ${ethereum.getBalance(account)} ETH`;
             }
             if (account == coinbase) {
                 console.log(`${out.red.bold} <- coinbase`)
