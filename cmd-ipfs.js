83e406290e3c46732506fe6516d311ff615ef7f0
ikonovalov <konovalov84@gmail.com>
2016-11-08 23:18:41 +0300

    Migration to FileForce class

--- cmd-ipfs.js
+++ cmd-ipfs.js
@@ -15,21 +15,6 @@ const FileForce = require('./lib/libfileforce');
 
 const fileForce = new FileForce(config);
 
-const ARROW = '\u2192';
-
-console.log('Prepare ETH account');
-const account = ask.account();
-const password = ask.password();
-
-function encryptFileTag(tag, selfKeyPair,  publicKey, cb) {
-    libcrypto.encryptObjectForParty(
-        tag,
-        selfKeyPair,
-        !publicKey ? selfKeyPair.publicKey : publicKey,
-        cb
-    );
-}
-
 module.exports = {
 
     /**
@@ -37,80 +22,11 @@ module.exports = {
      * @param path to file.
      */
     add: (path) => {
-        let originalStream = fs.createReadStream(path);
-        let onetimeKey = libcrypto.randomKey();
-
-        tmp.file({prefix: 'force-'}, (err, tmpFilePath) => {
-            if (err) throw err;
-
-            const encryptedStream = fs.createWriteStream(tmpFilePath);
-
-            libcrypto.encryptStream(originalStream, encryptedStream, onetimeKey, (encryptionParams) => {
-
-                ipfs.add(tmpFilePath, (error, result) => {
-                    if (!error) {
-                        let rootBlock = result[0];
-                        var selfKeyPair = fileForce.unlockKeys(account, password);
-
-                        let tag = {
-                            algorithm: encryptionParams.algorithm,
-                            iv: encryptionParams.iv,
-                            secret: onetimeKey.toString('hex'),
-                            ipfsHash: rootBlock.hash,
-                            ipfsHashSign: libcrypto.sign(rootBlock.hash, selfKeyPair.privateKey).toString('base64')
-                        };
-
-                        console.log('File added to IPFS with tag:');
-                        console.log(`${JSON.stringify(tag, null, 2)}`.blue);
-
-                        console.log(`Encrypting tag with ECDH using AES...`);
-
-                        encryptFileTag(tag, selfKeyPair, null, (tagEncryptionResult) => {
-
-                                let ownerAddress = libcrypto.publicToAddress(tagEncryptionResult.ownerPubKey);
-                                let partyAddress = libcrypto.publicToAddress(tagEncryptionResult.destPubKey);
-                                let encryptedSymmetricTag = tagEncryptionResult.encryptedTag.cipherText.toString('base64');
-                                let ownerPublicKey = '0x' + tagEncryptionResult.ownerPubKey.toString('hex');
-                                let hkdf = tagEncryptionResult.hkdf;
-                                let symmetricAlgorithm = tagEncryptionResult.encryptedTag.algorithm;
-                                let symmetricIv = tagEncryptionResult.encryptedTag.iv;
-
-                                let ecTag = {
-                                    ownerAddress: ownerAddress,
-                                    partyAddress: partyAddress,
-                                    tag: encryptedSymmetricTag,
-                                    tagEncryption: {
-                                        protocol: 'ECDH',
-                                        ownerPublicKey: ownerPublicKey,
-                                        kdf: {
-                                            name: 'hkdf',
-                                            params: hkdf,
-                                        },
-                                        encryption: {
-                                            algorithm: symmetricAlgorithm,
-                                            iv: symmetricIv
-                                        }
-                                    },
-                                };
-
-                                let ecTagJson = JSON.stringify(ecTag);
-
-                                ipfs.addBuffer(Buffer.from(ecTagJson, 'utf8'), (e, r) => {
-                                    let rb = r[0];
-                                    let ecTagHash = rb.hash;
-                                    console.log('ecTag stored in IPFS');
-                                    console.log(`ecTag ${ecTagHash} ${ARROW} File ${rootBlock.hash}`.red.bold);
-                                });
-                            }
-                        );
-
-                    } else {
-                        console.error(error);
-                    }
-                });
-
-            });
-        });
+        console.log('Prepare ETH account');
+        const account = ask.account();
+        const password = ask.password();
+        const selfKeyPair = fileForce.unlockKeys(account, password);
+        fileForce.add(path, selfKeyPair, selfKeyPair.publicKey);
     },
 
     cat: (hash) => {
